21dfb2516fc85faeaf605600dc530b60655f954c
anguskt <angus2517@gmail.com>
2025-05-18 18:36:43 +0800

    新增打地址就有經緯度，將api key寫入.env

--- cafeproject/cafesearch/views.py
+++ cafeproject/cafesearch/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Cafe
+from django.conf import settings
 
 def research_page(request):
     cafes = Cafe.objects.all()
-    return render(request, 'research.html', {"cafes": cafes})
+    return render(request, 'research.html', {
+        'cafes': cafes,
+        'google_api_key': settings.GOOGLE_MAPS_API_KEY
+    })
 
 def cafe_detail(request, slug):
     cafe = get_object_or_404(Cafe, slug=slug)
